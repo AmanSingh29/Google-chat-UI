572f0323a5c8d58a99707d3c47a95687b4a35555
Aman Singh <amansingh979429@gmail.com>
2023-11-13 10:40:26 +0530

    updated

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-// const goSpace = () => {
-//   let a = document.getElementsByClassName("new_chat")[0];
-//   a.style.display = "none";
-//   let b = document.getElementsByClassName("new_chat")[1];
-//   b.style.display = "block";
-// };
-// const goChat = () => {
-//   let a = document.getElementsByClassName("new_chat")[1];
-//   a.style.display = "none";
-//   let b = document.getElementsByClassName("new_chat")[0];
-//   b.style.display = "block";
-// };
-
 const ham = () => {
   let a = document.getElementsByClassName("hamburger")[0];
   let clicked = document.getElementsByClassName("hamclicked")[0];
   if (clicked) {
+    a.style.position = "absolute";
     a.style.transform = "translateX(-100%)";
     a.classList.remove("hamclicked");
   } else {
+    a.style.position = "static";
     a.style.transform = "translateX(0%)";
     a.classList.add("hamclicked");
   }
